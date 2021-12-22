1b4611c112d85de135db3cb0f08178555279e52e
Isaac <isaacdelalama1013@gmail.com>
2021-12-21 21:56:57 -0600

    added buttons to select file and output directory

--- main.py
+++ main.py
@@ -3,31 +3,57 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 import sys
 
-
 class App(QWidget):
     def __init__(self):
         super().__init__()
+        self.title = 'PDF Text Dumper'
+        self.width = 500
+        self.height = 100
+        self.initUI()
 
-        #title
-        self.setWindowTitle('PDF Text Dumper')
+    def initUI(self):
+        self.setWindowTitle(self.title)
 
         # fixed size
-        self.setFixedHeight(100)
-        self.setFixedWidth(500)
+        self.setFixedHeight(self.height)
+        self.setFixedWidth(self.width)
 
         # select source file - label
         self.label_srcfile = QLabel('Select Source File:', self)
         self.label_srcfile.move(5, 5)
 
+        # select source file diaglog box - button
+        self.selectFileButton = QPushButton(self)
+        self.selectFileButton.setText('Select File')
+        self.selectFileButton.move(5, 25)
+        self.selectFileButton.clicked.connect(self.selectPDF_clicked)
+        
+             
         # select outfile location - label
         self.label_outfile = QLabel('Select Output Directory:', self)
-        self.label_outfile.move(5, 35)
+        self.label_outfile.move(5, 50)
 
-        # show all widgets
+        # select source file diaglog box - button
+        self.selectFileButton = QPushButton(self)
+        self.selectFileButton.setText('Select Output Folder')
+        self.selectFileButton.move(5, 70)
+        self.selectFileButton.clicked.connect(self.selectOutput_clicked)
+        
+
         self.show()
 
+    def selectPDF_clicked(self, arg2):
+        filename = QFileDialog.getOpenFileName(self, 'Select PDF File', '/home/', 'PDF Files (*.pdf)')
+        self.label_srcfile.setText(f'Selected File: {filename[0]}')
+        self.label_srcfile.adjustSize()
+
+    def selectOutput_clicked(self, arg2):
+        folderpath = QFileDialog.getExistingDirectory(self, "Select Folder")
+        self.label_outfile.setText(f'Output Directory: {folderpath}')
+        self.label_outfile.adjustSize()
 
 if __name__ == '__main__':
+    # execute the program
     app = QApplication(sys.argv)
     ex = App()
     sys.exit(app.exec_())
