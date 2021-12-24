2beaabb0920fbcfd07aba29ec60b999050a0413a
Isaac <isaacdelalama1013@gmail.com>
2021-12-23 22:43:44 -0600

    implemented extract button

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 import sys
+import pdfplumber
 
 class App(QWidget):
     def __init__(self):
@@ -30,7 +31,6 @@ class App(QWidget):
         self.selectFileButton.setText('Select File')
         self.selectFileButton.move(5, 25)
         self.selectFileButton.clicked.connect(self.selectPDF_clicked)
-        
              
         # select outfile location - label
         self.label_outfile = QLabel('Selected Output Folder:', self)
@@ -48,22 +48,40 @@ class App(QWidget):
         self.extractButton.move(415, 75)
         self.extractButton.clicked.connect(self.extractButton_clicked)
 
-        
-
         self.show()
+
+    def TextExtractedUI(self):
+        message = QMessageBox()
+        message.setWindowTitle('Text Extracted')
+        message.setText('Text has been extracted to output_text.txt')
+        message.exec_()
 
     def selectPDF_clicked(self, arg2):
         self.sourceFileDir = QFileDialog.getOpenFileName(self, 'Select PDF File', '/home/', 'PDF Files (*.pdf)')
-        self.label_srcfile.setText(f'Selected File: {filename[0]}')
+        self.label_srcfile.setText(f'Selected File: {self.sourceFileDir[0]}')
         self.label_srcfile.adjustSize()
 
     def selectOutput_clicked(self, arg2):
         self.destFolderDir = QFileDialog.getExistingDirectory(self, "Select Folder")
-        self.label_outfile.setText(f'Output Directory: {folderpath}')
+        self.label_outfile.setText(f'Output Directory: {self.destFolderDir}')
         self.label_outfile.adjustSize()
 
     def extractButton_clicked(self, arg2):
-        raise NotImplementedError()
+        # make sure file and output directory are selected
+        if self.sourceFileDir == '':
+            raise NotImplementedError('FILE NOT SELECTED')
+        if self.destFolderDir == '':
+            raise NotImplementedError('OUTPUT FOLDER NOT SELECTED')
+
+
+        with pdfplumber.open(self.sourceFileDir[0]) as pdf:
+            outFile = open(f'{self.destFolderDir}/output_text.txt', 'w+')
+            for page in pdf.pages:
+                outFile.write(page.extract_text())
+            outFile.close()
+
+        self.TextExtractedUI()
+        
 
 if __name__ == '__main__':
     # execute the program
